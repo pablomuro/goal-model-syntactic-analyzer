9a2395abbc4305325bd81bba688b68dd8cc0011d
pablomuro <pablo.muro@gmail.com>
2021-10-02 12:24:11 -0300

    new dists and run-dist update

--- run-dist.sh
+++ run-dist.sh
@@ -2,56 +2,59 @@
 
 cd dist;
 
+
+program='goal-model-syntactic-analyzer-linux'
+
 # Day Life In Nursing Homes
 
-# ./goal-model-syntactic-analyzer "../examples/Day Life In Nursing Homes/hddl/DayLifeInNursingHomes.hddl"\
+# ./$program "../examples/Day Life In Nursing Homes/hddl/DayLifeInNursingHomes.hddl"\
 #  "../examples/Day Life In Nursing Homes/gm/CleaningRooms.txt"\
 #  "../examples/Day Life In Nursing Homes/configuration/configurationCleaningRooms.json"
 
-# ./goal-model-syntactic-analyzer "../examples/Day Life In Nursing Homes/hddl/DayLifeInNursingHomes.hddl"\
+# ./$program "../examples/Day Life In Nursing Homes/hddl/DayLifeInNursingHomes.hddl"\
 #  "../examples/Day Life In Nursing Homes/gm/MedicationDispensing.txt"\
 #  "../examples/Day Life In Nursing Homes/configuration/configurationMedicationDispensing.json"
 
-# ./goal-model-syntactic-analyzer "../examples/Day Life In Nursing Homes/hddl/DayLifeInNursingHomes.hddl"\
+# ./$program "../examples/Day Life In Nursing Homes/hddl/DayLifeInNursingHomes.hddl"\
 #  "../examples/Day Life In Nursing Homes/gm/RegularCheckUps.txt"\
 #  "../examples/Day Life In Nursing Homes/configuration/configurationRegularCheckUps.json"
 
 # Deliver Goods - Equipment
 
-# ./goal-model-syntactic-analyzer "../examples/Deliver Goods - Equipment/hddl/DeliverGoodsEqupments.hddl"\
+# ./$program "../examples/Deliver Goods - Equipment/hddl/DeliverGoodsEqupments.hddl"\
 #  "../examples/Deliver Goods - Equipment/gm/DeliverGoodsEquipment.txt"\
 #  "../examples/Deliver Goods - Equipment/configuration/configuration.json"
 
 # Food Logistics
 
-# ./goal-model-syntactic-analyzer "../examples/Food Logistics/hddl/FoodLogistics.hddl"\
+# ./$program "../examples/Food Logistics/hddl/FoodLogistics.hddl"\
 #  "../examples/Food Logistics/gm/FoodLogisticsDelivery.txt"\
 #  "../examples/Food Logistics/configuration/configurationDelivery.json"
 
-# ./goal-model-syntactic-analyzer "../examples/Food Logistics/hddl/FoodLogistics.hddl"\
+# ./$program "../examples/Food Logistics/hddl/FoodLogistics.hddl"\
 #  "../examples/Food Logistics/gm/FoodLogisticsPickup.txt"\
 #  "../examples/Food Logistics/configuration/configurationPickup.json"
 
 # Keeping Clean
 
-# ./goal-model-syntactic-analyzer "../examples/Keeping Clean/hddl/Keeping Clean.hddl"\
+# ./$program "../examples/Keeping Clean/hddl/Keeping Clean.hddl"\
 #  "../examples/Keeping Clean/gm/Keeping Clean.txt"\
 #  "../examples/Keeping Clean/configuration/configuration.json"
 
 # Lab Samples Logistics
 
-# ./goal-model-syntactic-analyzer "../examples/Lab Samples Logistics/hddl/LabSampleLogistics.hddl"\
+# ./$program "../examples/Lab Samples Logistics/hddl/LabSampleLogistics.hddl"\
 #  "../examples/Lab Samples Logistics/gm/LabSampleLogistics.txt"\
 #  "../examples/Lab Samples Logistics/configuration/configuration.json"
 
 # Seizure Recognition
 
-# ./goal-model-syntactic-analyzer "../examples/Seizure Recognition/hddl/SeizureRecognition.hddl"\
+# ./$program "../examples/Seizure Recognition/hddl/SeizureRecognition.hddl"\
 #  "../examples/Seizure Recognition/gm/SeizureRecognition.txt"\
 #  "../examples/Seizure Recognition/configuration/configuration.json"
 
 # Vital Signs Monitoring
 
-./goal-model-syntactic-analyzer "../examples/Vital Signs Monitoring/hddl/VitalSignsMonitoring.hddl"\
+./$program "../examples/Vital Signs Monitoring/hddl/VitalSignsMonitoring.hddl"\
  "../examples/Vital Signs Monitoring/gm/VitalSignsMonitoring.txt"\
  "../examples/Vital Signs Monitoring/configuration/configuration.json"
